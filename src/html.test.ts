154c9122fafcd56bacd0575458d9730ce1fdb53c
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2024-05-16 09:06:36 +0900

    feat: return elements, with partial typing, with optional query result

--- src/html.test.ts
+++ src/html.test.ts
@@ -1,20 +1,108 @@
-import { expect, expectTypeOf, test } from 'vitest';
-import { HTMLElement } from 'happy-dom';
+import { HTMLCollection } from "happy-dom"
 
-import { GlobalRegistrator } from '@happy-dom/global-registrator';
-GlobalRegistrator.register({
-  url: 'http://localhost:3000',
-  width: 1920,
-  height: 1080,
-});
+import { GlobalRegistrator } from "@happy-dom/global-registrator"
+GlobalRegistrator.register({ url: "http://localhost:3000" })
 
-import { htmlFn } from './html.js';
+import { describe, expect, expectTypeOf, test } from "vitest"
 
-test('is a function', () => {
-  console.log('type:', typeof htmlFn);
-  expectTypeOf(htmlFn).toBeFunction();
-});
+import { htmlFn } from "./html.js"
 
-test('returns element', () => {
-  expect(htmlFn('<div>Hi there</div>')).toBeInstanceOf(HTMLElement);
-});
+test("is a function", () => {
+	console.log("type:", typeof htmlFn)
+	expectTypeOf(htmlFn).toBeFunction()
+})
+
+test("returns items of elements", () => {
+	const result = htmlFn("<div>Hi there</div>")
+	const [el] = result
+	expect(el).toBeInstanceOf(HTMLElement)
+	expect(result).toHaveLength(1)
+})
+
+test.skip("returns list of elements", () => {
+	const result = htmlFn("<div>Hi there</div>")
+	expect(result).toBeInstanceOf(NodeList)
+})
+
+describe("returning many", () => {
+	test("returns many elements", () => {
+		const [divEl, pEl] = htmlFn("<div>Hi there,</div><p>I am here</p>")
+
+		expect((divEl as HTMLElement).tagName).toEqual("DIV")
+		expect((pEl as HTMLElement).tagName).toEqual("P")
+	})
+
+	test("returns multiples of elements and nodes", () => {
+		const [divEl, text, pEl] = htmlFn("<div>Hi</div> there, <p>I am here</p>")
+
+		expect((divEl as HTMLElement).tagName).toEqual("DIV")
+		expect((text as Text).wholeText).toEqual(" there, ")
+		expect((pEl as HTMLElement).tagName).toEqual("P")
+	})
+
+	describe("with types", () => {
+		test("returns many elementswith types", () => {
+			const [divEl, pEl] = htmlFn<[HTMLDivElement, HTMLParagraphElement]>("<div>Hi there,</div><p>I am here</p>")
+
+			expect(divEl.tagName).toEqual("DIV")
+			expect(pEl.tagName).toEqual("P")
+		})
+
+		test("returns multiples of elements and nodes", () => {
+			const [divEl, text, pEl] = htmlFn<[HTMLDivElement, Text, HTMLParagraphElement]>(
+				"<div>Hi</div> there, <p>I am here</p>",
+			)
+
+			expect(divEl.tagName).toEqual("DIV")
+			expect(text.wholeText).toEqual(" there, ")
+			expect(pEl.tagName).toEqual("P")
+		})
+	})
+})
+
+describe("return many with array of elements as input", () => {
+	test.only("returns many elements", () => {
+		const [divEl, text, pEl] = htmlFn(["<div>Hi</div>", " there, ", "<p>I am here</p>"])
+
+		expect(divEl.tagName).toEqual("DIV")
+		expect(text.wholeText).toEqual(" there, ")
+		expect(pEl.tagName).toEqual("P")
+	})
+})
+
+test("last argument may be an option", () => {
+	const result = htmlFn("<div>Hi there</div>", {})
+	const [el] = result
+	expect(el).toBeInstanceOf(HTMLElement)
+	expect(result).toHaveLength(1)
+})
+
+describe("nesting", () => {
+	test("create nested element", () => {
+		const [divEl] = htmlFn<[HTMLElement]>("<div>Hi there, <em>mate<em>!</div>")
+		expect(divEl).toBeInstanceOf(HTMLDivElement)
+
+		const emEl = divEl.querySelector("em")
+		expect(emEl).toBeInstanceOf(HTMLElement)
+	})
+
+	test("assign nested queries as option", () => {
+		const result = htmlFn("<div>Hi there, <em>mate<em>!</div>", {
+			query: {
+				emEl: "em",
+			},
+		})
+		const [divEl, { emEl }] = result
+		expect(divEl).toBeInstanceOf(HTMLDivElement)
+		expect(emEl).toBeInstanceOf(HTMLElement)
+	})
+})
+
+describe("attributes", () => {
+	test("assign attributes properly", () => {
+		const [button] = htmlFn<[HTMLButtonElement]>('<button type="button" aria-pressed="false">Click me</button>')
+
+		expect(button.getAttribute("type")).toEqual("button")
+		expect(button.getAttribute("aria-pressed")).toEqual("false")
+	})
+})
--- src/html.ts
+++ src/html.ts
@@ -1,4 +1,69 @@
-export function htmlFn(string: string) {
-  const el = document.createElement('div');
-  return el;
+type Options<Q extends object> = {
+	//returns: "element" | "node" | ("element" | "node")[];
+	query: Q;
+};
+
+type QueryResultOf<Q extends object> = {
+	[K in keyof Q]: HTMLElement | null;
+};
+
+//export function htmlFn<T extends Node[]>(htmlString: string, options: Partial<Options>): [...T, object];
+//export function htmlFn<T extends Node[], Q extends object>(htmlString: string, options: Omit<Options<Q>, "query">): T;
+
+/**
+ * Create HTML elements
+ */
+export function htmlFn<T extends Node[]>(htmlString: string): T;
+export function htmlFn<T extends Node[], Q extends object>(
+	htmlString: string,
+	options: { query: Q },
+): [...T, QueryResultOf<Q>];
+export function htmlFn<T extends Node[], Q extends object>(
+	htmlString: string,
+	options?: Partial<Options<Q>>,
+): T | [...T, QueryResultOf<Q>?] {
+	const el = document.createElement("div");
+	el.innerHTML = htmlString;
+
+	//const lastArg = moreArgs[moreArgs.length - 1];
+	//console.log("🚀 ~ file: html.ts:9 ~ htmlFn ~ lastArg:", lastArg);
+	//let options: Partial<Options> = {};
+	//if (typeof lastArg === "object") {
+	//	options = lastArg;
+	//}
+
+	const childNodes = el.childNodes;
+
+	const resultNodes = [...childNodes] as unknown as T;
+	//const resultNodes: T = [] as T;
+	//for (const node of childNodes) {
+	//	resultNodes.push(node);
+	//}
+	if (!options?.query) {
+		return resultNodes;
+	}
+
+	// assign query results
+	const queryResult: QueryResultOf<Q> = Object.entries(options.query).reduce(
+		(memo, [name, selector]) => {
+			memo[name as keyof Q] = el.querySelector<HTMLElement>(selector);
+			return memo;
+		},
+		{} as Record<keyof Q, HTMLElement | null>,
+	);
+	return [...resultNodes, queryResult];
+}
+
+export function htmlFnWithMultipleArgs<T extends Node[]>(
+	htmlString: string,
+	...moreArgs: string[] | [...string[], options: Partial<Options>]
+): [...T, object?] {
+	return [] as unknown as [...T, object?];
+}
+
+export function htmlNodeFn(string: string): NodeList {
+	const el = document.createElement("div");
+	el.innerHTML = string;
+
+	return el.childNodes;
 }
