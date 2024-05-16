7ec5184328c8c7e14d6ab9d5a37a60e8d5598581
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2024-05-17 00:11:55 +0900

    fix: export `html`

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { htmlFn } from "./html_unified.js";
 export { htmlSingleFn } from "./html_single.js";
 export { htmlMultipleFn } from "./html_multiple.js";
 export { htmlWithQueryFn } from "./html_with_query_option.js";
+export { html } from './html_tagged_templates.js';
