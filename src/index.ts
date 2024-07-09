95e6e97363f6036b40b783be3855bf5d29b3f371
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2024-07-09 17:42:27 +0900

    bump version to 0.3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 //export { htmlUnifiedFn } from "./html_unified.js";
 export { htmlSingleFn } from "./html_single.js";
 export { htmlMultipleFn } from "./html_multiple.js";
-export { htmlTupleFn } from "./html_tuple.js";
+export { htmlTupleFn, htmlQuery } from "./html_tuple.js";
 export { html } from "./html_tagged_templates.js";
+export { template } from "./html_template.js";
